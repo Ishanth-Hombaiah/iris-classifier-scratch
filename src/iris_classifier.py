beaef1c4820998c1d1c6145ba8abe1cf671ff64a
Ishanth-Hombaiah <hombaiah01@gmail.com>
2025-09-09 23:17:54 -0700

    Finished iris classifier

--- src/iris_classifier.py
+++ src/iris_classifier.py
@@ -1,12 +1,22 @@
-import torch
+import numpy as np
 from sklearn import datasets
 from logistic_regression_model import LogisticRegression
-from torch import nn
 
 iris = datasets.load_iris()
 
-model_0 = LogisticRegression(0.01, 1000)
+X, y = iris.data, iris.target
 
-loss_fn = nn.CrossEntropyLoss()
-optimizer =  torch.optim.SGD(model_0, lr=0.01)
+iris_classes = np.unique(y)
 
+models = []
+
+for i in iris_classes: # Training 3 models, one for each iris species, so we can compare their results for each input to determine which species each is
+    curr_y = (y == i).astype(int)
+    curr_model = LogisticRegression(0.01, 3000) 
+    curr_model.fit(X, curr_y, 26) 
+    models.append(curr_model)
+
+def ovr_predict(x):
+    x = np.asarray(x, dtype=float) # In case input is a 1d sample
+    probs = np.column_stack([model.predict_probs(x) for model in models])
+    return np.argmax(probs, axis=1)
